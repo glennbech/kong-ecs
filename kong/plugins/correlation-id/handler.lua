d62e01d8ae24832e80ddd286ad168ecfdafc8501
glennbech <glenn.bech@gmail.com>
2024-12-05 21:55:46 -0800

    asdasasd

--- kong/plugins/correlation-id/handler.lua
+++ kong/plugins/correlation-id/handler.lua
@@ -1,22 +1,16 @@
-local BasePlugin = require "kong.plugins.base_plugin"
-local uuid = require "resty.jit-uuid"
+local kong = kong
 
-local CorrelationIdHandler = BasePlugin:extend()
-
-CorrelationIdHandler.PRIORITY = 1000
-CorrelationIdHandler.VERSION = "1.0.0"
-
-function CorrelationIdHandler:new()
-  CorrelationIdHandler.super.new(self, "correlation-id")
-end
+local CorrelationIdHandler = {
+  PRIORITY = 1000,
+  VERSION = "1.0.0",
+}
 
 function CorrelationIdHandler:access(conf)
-  CorrelationIdHandler.super.access(self)
-  
   -- Get correlation ID from header or generate new one
   local correlation_id = kong.request.get_header(conf.header_name)
   if not correlation_id then
-    correlation_id = uuid.generate_v4()
+    -- Using a simple timestamp-based ID since we don't have uuid module
+    correlation_id = string.format("%d-%d", os.time(), math.random(1000000))
   end
   
   -- Set the correlation ID in headers for both request and response
